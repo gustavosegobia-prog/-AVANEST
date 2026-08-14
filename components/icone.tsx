f84d6a548d069266d2769c2480474bedcac3b953
Gustavo segobia <gustavosegobia@icloud.com>
2026-08-14 07:58:39 -0300

    Update icone.tsx

--- components/icone.tsx
+++ components/icone.tsx
@@ -26,6 +26,7 @@ const TRACOS = {
   imprimir: <><path d="M7 9V3.5h10V9"/><rect x="3.5" y="9" width="17" height="7" rx="2"/><path d="M7 14h10v6.5H7z"/></>,
   alerta: <><path d="M12 3.8 21 19.5H3z"/><path d="M12 9.5v4.5"/><path d="M12 17.2v.1"/></>,
   pausa: <><path d="M9.5 5v14"/><path d="M14.5 5v14"/></>,
+  seta: <path d="m6 9.5 6 6 6-6"/>,
   voltar: <><path d="M9.5 5.5 3.5 12l6 6.5"/><path d="M3.5 12h11a6 6 0 0 1 0 12h-1"/></>,
   copiar: <><rect x="8.5" y="8.5" width="12" height="12" rx="2"/><path d="M15.5 5.5h-9a2 2 0 0 0-2 2v9"/></>,
   whatsapp: <><path d="M20 12a8 8 0 0 1-11.9 7L4 20l1.1-4A8 8 0 1 1 20 12Z"/><path d="M9 9.5c0 3 2.5 5.5 5.5 5.5l1-1.4-2-1-.9.9a4.6 4.6 0 0 1-2.1-2.1l.9-.9-1-2z" fill="currentColor" stroke="none"/></>,
